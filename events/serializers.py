6d1c32fe29ff2691a6a506f00e215a34e4684d36
nonnymox <knonxho@gmail.com>
2025-01-11 09:36:03 -0800

    Modified endpoint for event registration

--- events/serializers.py
+++ events/serializers.py
@@ -10,11 +10,13 @@ class EventSerializer(serializers.ModelSerializer):
         read_only_fields = ['organizer', 'created_at']  # Ensure `created_by` and `created_at` are read-only
 
 class AttendeeSerializer(serializers.ModelSerializer):
+    event_title = serializers.CharField(source="event.title", read_only=True)  # Read-only field for event title
     class Meta:
         model = Attendee
-        fields = ['name', 'email', 'event']
+        fields = ['name', 'email', 'event_title']
 
 class WaitlistSerializer(serializers.ModelSerializer):
+    event_title = serializers.CharField(source="event.title", read_only=True)  # Read-only field for event title
     class Meta:
         model = Waitlist
-        fields = ['name', 'email', 'event']
+        fields = ['name', 'email', 'event_title']
