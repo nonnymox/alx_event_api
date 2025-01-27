1579ec381470050673a30a1e8ec4daa74e4c2917
Emmanuel Chukwunonso Ighavbota <121444985+nonnymox@users.noreply.github.com>
2025-01-27 03:56:37 -0800

    Updated views.py to reflect the  features of the api on the home page

--- alx_event_api/views.py
+++ alx_event_api/views.py
@@ -1,6 +1,18 @@
-# alx_event_api/views.py
-
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 def home(request):
-    return HttpResponse("Welcome to the homepage!")
+    return JsonResponse({
+        "message": "Welcome to the Event Management API",
+        "endpoints": [
+            "/api/register/ - Create new user",
+            "/api/users/ - Manage users",
+            "/api/users/{id} - GET - Retrieves the user information, PUT - Updates the user profile REQUIRES AUTHENTICATION TOKEN",
+            "/api/login/ - Login to user account(Get JWT access and refresh token for auth) ",
+            "/api/events/ - List upcoming events",
+            "/api/events/create/ - Create new event REQUIRES AUTHENTICATION TOKEN",
+            "/api/events/<int>/ - GET - Retrieves detials of a specific event, PUT - Perform CRUD operations on the events REQUIRES AUTHENTICATION TOKEN",
+            "api/events/<id>/capacity - GET - Retrieves the capacity of a specific event"
+            "/api/token/ - Obtain token",
+            "/api/token/refresh/ - Refresh token",
+        ]
+    })
